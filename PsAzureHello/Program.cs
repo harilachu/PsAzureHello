51507911d01af0eee9b2f399ada5e4f8f4b8d883
Hariprasad <hariprasad.becse@gmail.com>
2019-04-18 20:07:21 +0530

    Migrate database in Azure.

--- PsAzureHello/Program.cs
+++ PsAzureHello/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PsAzureHello.Data;
 
 namespace PsAzureHello
 {
@@ -14,12 +17,28 @@ namespace PsAzureHello
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var webHost = CreateWebHostBuilder(args).Build();
+            MigrateDatabase(webHost);
+            webHost.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseApplicationInsights()
                 .UseStartup<Startup>();
+
+        /// <summary>
+        /// Migrates the database in Azure.
+        /// </summary>
+        /// <param name="host"></param>
+        public static void MigrateDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider
+                    .GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+        }
     }
 }
